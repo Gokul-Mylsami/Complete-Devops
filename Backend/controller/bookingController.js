e794e8622d0568c4fac98bddbdce8c7d53c76c6f
Gokul-Mylsami <gokulmylsami@gmail.com>
2023-12-17 12:08:34 +0530

    prom bug fixed

--- Backend/controller/bookingController.js
+++ Backend/controller/bookingController.js
@@ -187,9 +187,9 @@ exports.deleteBooking = catchAsync(async (req, res, next) => {
 
   // total_bookings_today.;
   total_no_tickets_cancelled.inc();
-  total_no_of_economy_seats_booked.dec(bookedEconomySeats);
-  total_no_of_premium_class_seats_booked.dec(bookedPremiumClassSeats);
-  total_no_of_first_class_seats_booked.dec(bookedFirstClassSeats);
+  // total_no_of_economy_seats_booked.dec(bookedEconomySeats);
+  // total_no_of_premium_class_seats_booked.dec(bookedPremiumClassSeats);
+  // total_no_of_first_class_seats_booked.dec(bookedFirstClassSeats);
 
   await new Email(booking.user).sendBookingCancellation(booking);
   res.status(200).json({
